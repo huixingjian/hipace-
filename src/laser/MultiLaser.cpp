41b9876d72eb6b194891fb365acc5b2aa099a853
huixingjian <xingjian.hui@desy.de>
2024-03-06 12:37:35 +0100

    check the axislabel fo hipace output

--- src/laser/MultiLaser.cpp
+++ src/laser/MultiLaser.cpp
@@ -276,7 +276,8 @@ MultiLaser::GetEnvelopeFromFile (const amrex::Geometry& gm) {
     } else if (axis_labels[0] == "t" && axis_labels[1] == "r") {
         m_file_geometry = "rt";
     } else {
-        amrex::Abort("Incorrect axis labels in laser file, must be either t, y, x or t, r");
+        //amrex::Abort("Incorrect axis labels in laser file, must be either t, y, x or t, r");
+        amrex::Abort(axis_labels)
     }
 
     const std::shared_ptr<input_type> data = laser_comp.loadChunk<input_type>();
