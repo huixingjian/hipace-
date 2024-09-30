cd025f90598eb1668d1ab973eaafc2e2cbbc2583
huixingjian <xingjian.hui@desy.de>
2024-09-30 19:06:37 +0200

    error in step 0

--- src/laser/MultiLaser.cpp
+++ src/laser/MultiLaser.cpp
@@ -1056,8 +1056,8 @@ MultiLaser::AdvanceSliceFFT (const amrex::Real dt, int step)
                         + 2._rt * arr(i, j, chi) * an00j00
                         - lapA
                         + ( -6._rt/(c*dt*dz) + 4._rt*I*djn/(c*dt) + I*4._rt*k0/(c*dt) ) * an00j00;
-                    arr(i, j, comp_rhs_r) = + 4._rt/(c*dt*dz)*(-anp1jp1+anm1jp1)*exp1
-                                        + 1._rt/(c*dt*dz)*(+anp1jp2-anm1jp2)*exp2;
+                    arr(i, j, comp_rhs_r) = + 8._rt/(c*dt*dz)*(-anp1jp1+an00jp1)*exp1
+                                            + 2._rt/(c*dt*dz)*(+anp1jp2-an00jp2)*exp2;
                     arr(i, j, comp_rhs_i) = 0;
                 } else {
                     const Complex anm1jp1 = arr(i, j, nm1jp1_r) + I * arr(i, j, nm1jp1_i);
