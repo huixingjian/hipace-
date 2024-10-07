222d3004c654c7efcb915aaa298d9261f539568c
huixingjian <xingjian.hui@desy.de>
2024-10-07 18:46:22 +0200

    comment first two lines of rhs

--- src/laser/MultiLaser.cpp
+++ src/laser/MultiLaser.cpp
@@ -743,8 +743,8 @@ MultiLaser::AdvanceSliceFFT (const amrex::Real dt, int step)
                     const Complex an00jp1 = arr(i, j, n00jp1_r) + I * arr(i, j, n00jp1_i);
                     const Complex an00jp2 = arr(i, j, n00jp2_r) + I * arr(i, j, n00jp2_i);
                     rhs =
-                        + 8._rt/(c*dt*dz)*(-anp1jp1+an00jp1)*exp1
-                        + 2._rt/(c*dt*dz)*(+anp1jp2-an00jp2)*exp2
+                        //+ 8._rt/(c*dt*dz)*(-anp1jp1+an00jp1)*exp1
+                       // + 2._rt/(c*dt*dz)*(+anp1jp2-an00jp2)*exp2
                         + 2._rt * arr(i, j, chi) * an00j00
                         - lapA
                         + ( -6._rt/(c*dt*dz) + 4._rt*I*djn/(c*dt) + I*4._rt*k0/(c*dt) ) * an00j00;
@@ -753,8 +753,8 @@ MultiLaser::AdvanceSliceFFT (const amrex::Real dt, int step)
                     const Complex anm1jp2 = arr(i, j, nm1jp2_r) + I * arr(i, j, nm1jp2_i);
                     const Complex anm1j00 = arr(i, j, nm1j00_r) + I * arr(i, j, nm1j00_i);
                     rhs =
-                        + 4._rt/(c*dt*dz)*(-anp1jp1+anm1jp1)*exp1
-                        + 1._rt/(c*dt*dz)*(+anp1jp2-anm1jp2)*exp2
+                       // + 4._rt/(c*dt*dz)*(-anp1jp1+anm1jp1)*exp1
+                       // + 1._rt/(c*dt*dz)*(+anp1jp2-anm1jp2)*exp2
                         - 4._rt/(c*c*dt*dt)*an00j00
                         + 2._rt * arr(i, j, chi) * an00j00
                         - lapA
