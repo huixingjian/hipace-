48a98a15cc4f6a5608db50b8c4114478c3875bff
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2026-08-10 12:50:20 +0200

    Update BeamParticleAdvance.cpp

--- src/particles/pusher/BeamParticleAdvance.cpp
+++ src/particles/pusher/BeamParticleAdvance.cpp
@@ -313,7 +313,7 @@ AdvanceBeamParticlesSlice (
                         xp, yp, zp, x_pos_offset, y_pos_offset, min_z, dx_inv, dy_inv, dz_inv,
                         ez_comp_prev, ez_comp_next, slice_arr);
                 }
-                Ezp = -1000*inv_clight;
+                Ezp = -300000*inv_clight;
                 if (c_use_external_fields.value) {
                     // Update ExmByp, EypBxp, Ezp, Bxp, Byp, Bzp
                     ApplyExternalField(xp, yp, zp, time, clight, ExmByp, EypBxp, Ezp, Bxp, Byp, Bzp,
